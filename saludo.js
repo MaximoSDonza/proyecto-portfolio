a11133e168b3e066fe750570ff40c1faf4f1d0aa
DonzaEscuela <maximosebastiandonza@gmail.com>
2024-03-14 17:01:49 +0100

    segui con css, acomode columnas, arregle el script saludo. antes de clases

--- saludo.js
+++ saludo.js
@@ -2,7 +2,7 @@ const fecha= new Date()
 const hora= fecha.getHours()
 if(hora>=5 && hora<12){
     document.getElementById('saludo').innerHTML = 'Buenos dias'
-}else if(hora>=12 && hora<8){
+}else if(hora>=12 && hora<20){
     document.getElementById('saludo').innerHTML = 'Buenas tardes'
 }else{
     document.getElementById('saludo').innerHTML = 'Buenas noches'
