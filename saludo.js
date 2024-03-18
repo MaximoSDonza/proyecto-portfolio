396cc0a5acc9134112a4dd1ec82db5b54b73f432
DonzaEscuela <maximosebastiandonza@gmail.com>
2024-03-18 18:12:55 +0100

    mezcla de colores intento de aplicacion de paleta de colores dependiendo de la hora del dia. toque JS y acomode un p en css

--- saludo.js
+++ saludo.js
@@ -1,11 +1,24 @@
 const fecha= new Date()
 const hora= fecha.getHours()
+const body= document.querySelector('body')
+const costado= document.getElementById('costado')
+const descripcion= document.getElementById('descripcion')
+const a= document.querySelector('a')
 if(hora>=5 && hora<12){
     document.getElementById('saludo').innerHTML = 'Buenos dias'
+    body.style.backgroundColor='#a4dcb9'
 }else if(hora>=12 && hora<20){
     document.getElementById('saludo').innerHTML = 'Buenas tardes'
+
 }else{
     document.getElementById('saludo').innerHTML = 'Buenas noches'
-}       
 
-console.log(hora)
+}
+a.style.borderColor='#b7ffea'
+saludo.style.backgroundColor='#ecffff'
+saludo.style.borderColor='#d1fff4'       
+descripcion.style.backgroundColor='#ecffff'
+descripcion.style.borderColor='#d1fff4'
+costado.style.backgroundColor='#ecffff'
+costado.style.borderColor='#d1fff4'
+body.style.backgroundColor='#b7ffea'
