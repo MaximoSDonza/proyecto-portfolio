b15b4e68267954496b4e52077acedab1c8237c7f
Donza <maximosebastiandonza@gmail.com>
2024-03-26 09:45:29 -0300

    solucionado responsive, agregada pantalla carga. cambiar imagenes y crear dropdown

---
+++ carga.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    const contenedor = document.getElementById('contenedor_carga');
+
+    contenedor.style.visibility= 'hidden';
+    contenedor.style.opacity= '0';
+}
--- saludo.js
+++ saludo.js
@@ -4,17 +4,17 @@ const body= document.querySelector('body')
 const root= document.querySelector(':root')
 
 if(hora>=5 && hora<12){
-    document.getElementById('saludo').innerHTML = 'Buenos dias'
+    document.getElementById('saludo').innerHTML = 'Buenos Dias'
     root.style.setProperty('--bgcolor','#daffff');
     root.style.setProperty('--bcolor','#c0ecf9');
     root.style.setProperty('--bodycolor','#a7d9f3');
 }else if(hora>=12 && hora<20){
-    document.getElementById('saludo').innerHTML = 'Buenas tardes'
+    document.getElementById('saludo').innerHTML = 'Buenas Tardes'
     root.style.setProperty('--bgcolor','#ffffb6');
     root.style.setProperty('--bcolor','#efdd6c');
     root.style.setProperty('--bodycolor','#e9d253');
 }else{
-    document.getElementById('saludo').innerHTML = 'Buenas noches'
+    document.getElementById('saludo').innerHTML = 'Buenas Noches'
     root.style.setProperty('--bgcolor','#a7a7a7');
     root.style.setProperty('--bcolor','#545454');
     root.style.setProperty('--bodycolor','#2c2c2c');
